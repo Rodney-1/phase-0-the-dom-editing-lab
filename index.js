618b608f8dc74e8b08d06c51554a0e0d89493ead
Rodney_Karani <rodneykarani07@gmail.com>
2025-06-27 23:53:20 +0300

    Complete DOM HTML lab

---
+++ index.js
@@ -0,0 +1,12 @@
+// Write your code here!
+const main = document.getElementById('main');
+if (main) {
+    main.remove ();
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+newHeader.textContent = 'Rodney is the champion'
+document.body.append (newHeader);
